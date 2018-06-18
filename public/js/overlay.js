2baa77d5279bd662b8a799a66e086a4b266c829e
Cora Yiu <cora@Coras-MBP-2.hitronhub.home>
2018-06-18 15:29:55 -0700

    bl

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -1,4 +1,14 @@
 $('#toggle').click(function() {
     $(this).toggleClass('toggle-active');
     $('#overlay').toggleClass('nav-active');
+    
+     $("button").on("click", function() {
+  var el = $(this);
+  if (el.text() == el.data("text-swap")) {
+    el.text(el.data("text-original"));
+  } else {
+    el.data("text-original", el.text());
+    el.text(el.data("text-swap"));
+  }
+});
 });
